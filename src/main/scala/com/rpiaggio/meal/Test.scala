71bcaa1ce990e249c14bd622127e724ceb1176cb
Raúl Piaggio <rpiaggio@gmail.com>
2019-05-14 03:39:40 -0300

    Serving!

--- src/main/scala/com/rpiaggio/meal/Test.scala
+++ src/main/scala/com/rpiaggio/meal/Test.scala
@@ -1,10 +1,19 @@
 package com.rpiaggio.meal
 
-import cats.effect.{ContextShift, ExitCode, IO, IOApp, Timer}
-import com.github.gvolpe.http4s.StreamUtils
+import cats.effect._
+import cats.effect.concurrent.Ref
+import cats.implicits._
+import fs2.Stream
+import fs2.concurrent.SignallingRef
+import org.http4s.{Charset, HttpRoutes, MediaType}
+import org.http4s.syntax._
+import org.http4s.dsl.io._
+import org.http4s.implicits._
+import org.http4s.server.blaze._
+import org.http4s.server.Router
+import org.http4s.headers.`Content-Type`
 
 import scala.concurrent.ExecutionContext.Implicits.global
-
 
 
 object Test extends IOApp {
@@ -17,13 +26,27 @@ object Test extends IOApp {
 
   val outputStream = RssRenderer(feed.channelEntry, client.stream(feed.channelEntry.link).through(feed.parser).through(feed.formatter))
 
-  val utils = implicitly[StreamUtils[IO]]
+  val service = HttpRoutes.of[IO] {
+    case _ => Ok(outputStream, `Content-Type`(MediaType.application.`rss+xml`, Charset.`UTF-8`))
+  }
 
-  val out =
-    for {
-      response <- outputStream
-      _ <- utils.putStr(response.toString)
-    } yield ()
-
-  override def run(args: List[String]) = out.compile.drain.map(_ => ExitCode.Success)
+  override def run(args: List[String]) = {
+//    val server =
+      BlazeServerBuilder[IO]
+        .bindHttp(8080, "localhost")
+        .withHttpApp(Router("/" -> service).orNotFound)
+//
+//    val httpService =
+//      for {
+//        signal <- Stream.eval(SignallingRef[IO, Boolean]{System.in.read(); true})
+//        exitCode <- Stream.eval(Ref[IO].of(ExitCode.Success))
+//        serve <- server.serveWhile(signal, exitCode)
+//      } yield serve
+//
+//    httpService
+      .serve
+      .compile
+      .drain
+      .as(ExitCode.Success)
+  }
 }
