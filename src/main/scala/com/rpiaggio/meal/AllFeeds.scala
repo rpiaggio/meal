8489a0fa39493154bd9429278aef408976e22df8
Raúl Piaggio <rpiaggio@gmail.com>
2019-06-17 18:39:42 -0300

    Dynamic feeds working.

---
+++ src/main/scala/com/rpiaggio/meal/AllFeeds.scala
@@ -0,0 +1,5 @@
+package com.rpiaggio.meal
+
+object AllFeeds extends FeedList {
+  val feeds = uy.TickAntel.feeds ++ uy.RedUTS.feeds ++ uy.Abitab.feeds ++ uy.DSM.feeds ++ uy.RedTickets.feeds
+}
--- src/main/scala/com/rpiaggio/meal/Test.scala
+++ src/main/scala/com/rpiaggio/meal/Test.scala
@@ -11,7 +11,6 @@ import org.http4s.server.blaze._
 import org.http4s.server.Router
 import org.http4s.headers.`Content-Type`
 
-import scala.annotation.tailrec
 import scala.concurrent.ExecutionContext.Implicits.global
 
 
@@ -19,11 +18,9 @@ object Test extends IOApp {
   implicit val cs: ContextShift[IO] = IO.contextShift(global)
   implicit override val timer: Timer[IO] = IO.timer(global)
 
-  val feed = uy.TickAntel.feeds.head._2
-
   val client = new HttpClient[IO]
 
-  val outputStream = RssRenderer(feed.channelEntry,
+  def render(feed: Feed) = RssRenderer(feed.channelEntry,
     client.stream(feed.channelEntry.link)
       .through(feed.parser)
       .through(feed.formatter)
@@ -34,99 +31,27 @@ object Test extends IOApp {
   )
 
   val service = HttpRoutes.of[IO] {
-    case _ => Ok(outputStream, `Content-Type`(MediaType.application.`rss+xml`, Charset.`UTF-8`))
+    case GET -> Root / feedName =>
+      AllFeeds.feeds.get(feedName).fold(NotFound("Invalid feed name."))(feed =>
+        Ok(render(feed), `Content-Type`(MediaType.application.`rss+xml`, Charset.`UTF-8`))
+      )
   }
 
   override def run(args: List[String]) = {
+    val host = "localhost"
+    val port = 8080
+
+    println(s"Server online at http://$host:$port/")
+    println("Valid feeds:")
+    AllFeeds.feeds.keys.toSeq.sorted.foreach(key => println(s"* $key"))
+
     //    val server =
     BlazeServerBuilder[IO]
-      .bindHttp(8080, "localhost")
+      .bindHttp(port, host)
       .withHttpApp(Router("/" -> service).orNotFound)
-      //
-      //    val httpService =
-      //      for {
-      //        signal <- Stream.eval(SignallingRef[IO, Boolean]{System.in.read(); true})
-      //        exitCode <- Stream.eval(Ref[IO].of(ExitCode.Success))
-      //        serve <- server.serveWhile(signal, exitCode)
-      //      } yield serve
-      //
-      //    httpService
       .serve
       .compile
       .drain
       .as(ExitCode.Success)
-  }
-
-  def run2(args: List[String]) = {
-    import cats._
-    import cats.implicits._
-    import fs2._
-    import scala.concurrent.duration._
-
-    //    val p = many(digit)
-
-    val stream = Stream.emits[IO, String](List("1", "x2f", "e z", "3", "f", "g", "h", "y")).metered(1.seconds)
-    //
-    //    val stream = Stream.emit[IO, String]("12e3f")
-
-    //    val p = for { n <- int; c <- take(n) } yield c
-
-    //    val p = /*string("x2") <~ many(anyChar) <~*/ string("x")
-
-    //    val p = bracket(string("x2"), takeWhile(_ => true), string("3"))
-
-    val xxx =
-      stream
-        .through(_.map { x => println(s"< $x"); x })
-        //          .delayBy(
-        //        .through(parseBetween(p))
-        .through(_.map { x => println(s">> $x"); x })
-        .compile
-        .toList
-    //        .toRight("Nothing Emited")
-    //        .flatMap(_.either)
-
-    xxx.map { r =>
-      println(r)
-      ExitCode.Success
-    }
-
-    //    ???
-  }
-
-  def run3(args: List[String]) = {
-    IO {
-      //      val p = "ababababca"
-      //      println(computeKMPPi(p))
-
-      ExitCode.Success
-    }
-  }
-
-  def run4(args: List[String]) = {
-    import cats._
-    import cats.implicits._
-    import fs2._
-    import scala.concurrent.duration._
-
-
-    val pattern = ParsePattern(List(ParseUntil(ParseAction.Ignore, ">>>"), ParseUntil(ParseAction.Capture, "<<<")))
-    val parser = EntityParser[IO](pattern)
-
-    val stream = Stream.emits[IO, String](List("1", "x2f", "e z", ">>", ">HO", "LA!", "<", "<<", "je", ">", ">", ">", ">", ">", ":", ")", "<", "<", "OSO<<<")).metered(1.seconds)
-
-
-    val xxx =
-      stream
-        .through(_.map { x => println(s"< $x"); x })
-        .through(parser)
-        .through(_.map { x => println(s">> $x"); x })
-        .compile
-        .toList
-
-    xxx.map { r =>
-      println(r)
-      ExitCode.Success
-    }
   }
 }
